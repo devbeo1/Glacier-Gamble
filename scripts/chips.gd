57eb5a7990960abc0f57ca871d2df40f1bdb2307
frostyhill <devbeo12@gmail.com>
2025-07-03 14:54:10 +0100

    Update chips.gd

--- scripts/chips.gd
+++ scripts/chips.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var turn_speed:float = 0.5
+@export var turn_speed:float = 80
 
 func _process(delta: float) -> void:
-	rotation.y += deg_to_rad(turn_speed)+delta
+	rotation.y += deg_to_rad(turn_speed) * delta
